a78dee608855d592191feddbeb06782f0b383351
Noam Levin <155658672+noamlevin8@users.noreply.github.com>
2024-03-21 20:54:03 +0200

    rudp commit

--- RUDP_API.h
+++ RUDP_API.h
@@ -9,6 +9,9 @@
 #include <netinet/udp.h>
 #include <time.h>
 
+#define BUFFER_SIZE 1024
+
+
 typedef struct _flags
 {
     unsigned short int ACK :1;
@@ -16,31 +19,101 @@ typedef struct _flags
     unsigned short int FIN :1;
 }flags, *p_flags;
 
+
 typedef struct _rudp_packet
 {
     struct flags pocket_flags;
-    unsigned int seq;
-    unsigned short int checksum;    
+    char data[BUFFER_SIZE]; 
     unsigned short int length;
-    char data[BUFFER_SIZE];     
-  
+    unsigned short int checksum; 
+    unsigned int sequence;     
 }rudp_pack, *p_rudp_pack;
 
-typedef struct _rudp_socket{
+
+typedef struct _rudp_socket
+{
+    struct sockaddr_in destination_addr;
     int socket_fd;
-    int isServer;
-    int isConnected;
-    struct sockaddr_in dest_addr;
+    int Server;
+    int Connection;
 }RUDP_Sock, *p_RUDP_Sock;
 
+
 // Create RUDP socket
-int rudp_socket();
+p_RUDP_Sock rudp_socket(unsigned short int listen_port, int if_server);
 
-// Send after connection establishment
-int rudp_send(int socket);
 
-// Receiving data
-int rudp_recv(int socket);
+// Client asking server to connect
+int rudp_connect(p_RUDP_Sock sock_fd, const char* dest_ip, unsigned short int dest_port);
 
-// Close socket
-int rudp_close(int socket);
+
+// Server accepts connection from client
+int rudp_accept(p_RUDP_Sock sock_fd);
+
+
+// Send packet after connection establishment
+int rudp_send(p_RUDP_Sock sock_fd, p_rudp_pack pack);
+
+
+// Receiving from established connection
+int rudp_recv(p_RUDP_Sock sock_fd, p_rudp_pack pack, p_rudp_pack prev_pack); 
+
+
+// Disconnecting from an active connection
+int rudp_disconnect(p_RUDP_Sock sock_fd, int seq); 
+
+
+// Close the socket and free all the allocated memory
+int rudp_close(p_RUDP_Sock sock_fd); 
+
+
+// Create empty packet
+p_rudp_pack create_packet();
+
+
+// Creating a data pocket 
+void data_packet(p_rudp_pack pack, int seq, char* data);
+
+
+// Creating an acknowledgment packet 
+void ACK_packet(p_rudp_pack pack, int seq);
+
+
+// Creating a FIN packet  
+void FIN_packet(p_rudp_pack pack, int seq);
+
+
+// Copying one packet to another
+void copy_packet(p_rudp_pack pack_1, p_rudp_pack pack_2);
+
+
+// 
+//int create_socket();
+
+
+// Starting a handshake between the client and the server
+int handshake(p_RUDP_Sock sock_fd);
+
+
+// Sanding a packet again when an ack on the pocket didn't arrive
+int packet_resend(p_RUDP_Sock sock_fd, p_rudp_pack pack);
+
+
+// Sending SYN-ACK
+int send_SYN_ACK(p_RUDP_Sock sock_fd, int seq);
+
+
+// Sending ACK
+int send_ACK(p_RUDP_Sock sock_fd, int seq);
+
+
+// Sending FIN-ACK
+int send_FIN_ACK(p_RUDP_Sock sock_fd, int seq);
+
+
+// Sending FIN
+int send_FIN(p_RUDP_Sock sock_fd, p_rudp_pack pack);
+
+
+// Calculating the checksum of the packet
+unsigned short int calculate_checksum(void* data, unsigned int bytes);
